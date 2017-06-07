7c8ac74ea563ea41a4f75d0f61e6f04871fbd47c
Pascal Loose <pascal.loose@me.com>
2017-06-07 14:44:30 +0100

    Added threshold values

--- simple-mqtt.js
+++ simple-mqtt.js
@@ -13,19 +13,19 @@
   Bare Conductive code written by Szymon Kaliski.
 
   This work is licensed under a MIT license https://opensource.org/licenses/MIT
-  
+
   Copyright (c) 2016, Bare Conductive
-  
+
   Permission is hereby granted, free of charge, to any person obtaining a copy
   of this software and associated documentation files (the "Software"), to deal
   in the Software without restriction, including without limitation the rights
   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
   copies of the Software, and to permit persons to whom the Software is
   furnished to do so, subject to the following conditions:
-  
+
   The above copyright notice and this permission notice shall be included in all
   copies or substantial portions of the Software.
-  
+
   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -41,6 +41,14 @@ var mqtt   = require('mqtt');
 var argv   = require('yargs').argv;
 
 var mpr121;
+
+// this is the touch threshold - setting it low makes it more like a proximity trigger
+// default value is 40 for touch
+var touchThreshold = 40;
+
+// this is the release threshold - must ALWAYS be smaller than the touch threshold
+// default value is 20 for touch
+var releaseThreshold = 20;
 
 function printHelp() {
   console.log('Sends Pi Cap touch readings through MQTT - MUST be run as root.\n');
@@ -64,6 +72,9 @@ var client   = mqtt.connect('mqtt://' + broker, { username: username, password: 
 
 // correct address for the Pi Cap - other boards may vary
 mpr121 = new MPR121('0x5C');
+
+mpr121.setTouchThreshold(touchThreshold);
+mpr121.setReleaseThreshold(releaseThreshold);
 
 client.on('connect', function() {
   mpr121.on('data', function(data) {
@@ -93,4 +104,3 @@ client.on('connect', function() {
 process.on('SIGINT', function () {
   process.exit(0);
 });
-
